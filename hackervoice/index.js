2d63d04eff2a5759c9ecaa5146bdceace4eadf32
Steven Yeung <steveny1215@gmail.com>
2022-06-15 16:19:31 -0400

    trying

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,19 +1,16 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    //param name is password
+    context.log("JavaScript HTTP trigger function processed a request.");
+  
+    // param name = password
     var password = req.query.password;
     context.log(password);
-
+  
     let result = "";
-    if(password == "letmein"){
-        result = "Access granted."
-    }else{
-        result = "Access denied."
-    }
-    
+  
+    result = password == "letmein" ? "Access granted." : "Access denied.";
+  
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: result
+      // status: 200, /* Defaults to 200 */
+      body: result,
     };
-}
+  };
