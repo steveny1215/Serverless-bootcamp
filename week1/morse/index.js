9c849149a7ea4dae7e0f9f4ef4eb80b84d080f5e
Steven Yeung <steveny1215@gmail.com>
2022-06-17 23:17:12 -0400

    HI

--- week1/morse/index.js
+++ week1/morse/index.js
@@ -1,17 +1,17 @@
-const morse = require("morse-code-converter");
+
+const morse = require("morse-code-converter")
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const english = req.query.plaintext;
-    var code = "";
-
-    if (typeof english === 'undefined' || english === "") {
-        code = "Please enter some text to  convert!"
-    }else {
-        code = morse.textToMorse(english);
+    let plaintext = req.query.plaintext;
+    let code = undefined;
+    if (typeof plaintext === 'undefined' || plaintext === "") {
+        code = "Please enter some text to convert!"
     }
-
-
+    else {
+        code = morse.textToMorse(plaintext);
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
