51b873105dcea5b43e4d7a29949043e730a2cff7
Steven Yeung <steveny1215@gmail.com>
2022-06-18 19:16:58 -0400

    fixing

--- week1/emotionalgifs/index.js
+++ week1/emotionalgifs/index.js
@@ -1,21 +1,14 @@
-const multipart = require('parse-multipart');
-
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    const multipart = require('parse-multipart');
 
-    // here's your boundary:
     const boundary = multipart.getBoundary(req.headers['content-type']);
-
-    // TODO: assign the body variable the correct value
     const body = req.body;
-
-    // parse the body
     const parts = multipart.Parse(body, boundary);
 
-    let convertedResult = Buffer.from(parts[0].data).toString('base64');
-    // FILL IN THE BLANK
+    let result = Buffer.from(parts[0].data).toString('base64');
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: result
     };
 }
