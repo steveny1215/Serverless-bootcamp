694a6184e9d23961bdc692fa0f4dd4f36928e67a
Steven Yeung <steveny1215@gmail.com>
2022-06-19 17:55:13 -0400

    add emotional gifs

--- week1/emotionalgifs/index.js
+++ week1/emotionalgifs/index.js
@@ -1,4 +1,5 @@
 const multipart = require("parse-multipart");
+const fetch = require('node-fetch');
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
@@ -9,10 +10,39 @@ module.exports = async function (context, req) {
   // here's your parts:
   const parts = multipart.Parse(body, boundary);
   //Buffer the process of preloading data into a reserved area of memory
-  let convertedResult = Buffer.from(parts[0].data).toString('base64');
-
+  //module.exports function
+  //analyze the image
+  const result = await analyzeImage(parts[0].data);
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: convertedResult,
+	  body: {
+		  result
+	  }
   };
-};
+  console.log(result)
+  context.done(); 
+}
+
+
+
+async function analyzeImage(img){
+  const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+
+  const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+  let params = new URLSearchParams({
+    'returnFaceId': 'true',
+    'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+  })
+  let resp = await fetch(uriBase + '?' + params.toString(), {
+    method: 'POST',  //WHAT TYPE OF REQUEST?
+    body: img,  //WHAT ARE WE SENDING TO THE API?
+    headers: {
+        'Content-Type': 'application/octet-stream',  //do this in the next section
+        'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+  })
+  let data = await resp.json();
+
+  return data; 
+
+}
