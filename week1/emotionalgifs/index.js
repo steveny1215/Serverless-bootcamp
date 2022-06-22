69ecfee4531b6cbb41c22986e24906def267176f
Steven Yeung <steveny1215@gmail.com>
2022-06-22 13:56:37 -0400

    trying

--- week1/emotionalgifs/index.js
+++ week1/emotionalgifs/index.js
@@ -1,49 +1,56 @@
+const multipart = require('parse-multipart');
+const fetch = require('node-fetch')
 
-const multipart = require("parse-multipart");
-const fetch = require('node-fetch');
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-  // When the data starts and when the data ends
-  const boundary = multipart.getBoundary(req.headers['content-type']);
-  //We'll be specify a body parser for the multipart data
-  const body = req.body;
-  // here's your parts:
-  const parts = multipart.Parse(body, boundary);
-  //Buffer the process of preloading data into a reserved area of memory
-  //module.exports function
-  //analyze the image
-  const result = await analyzeImage(parts[0].data);
-  context.res = {
-	  body: {
-		  result
-	  }
-  };
-  console.log(result)
-  context.done(); 
+    const boundary = multipart.getBoundary(req.headers['content-type']);
+  
+    // Assigns the body variable the correct value
+    const body = req.body;
+
+    // parses the body
+    const parts = multipart.Parse(body, boundary);
+
+    // module.exports function
+    // analyzes the image
+    const result = await analyzeImage(parts[0].data);
+
+    /*
+    let emotions = result[0].faceAttributes.emotion;    
+    let objects = Object.values(emotions);
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+*/
+    context.res = {
+        body: {
+          result
+          //main_emotion
+
+        }
+    };
+    console.log(result)
+    context.done(); 
 }
 
+async function analyzeImage(img) {
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
 
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'  //FILL IN THIS LINE
+    })
 
-async function analyzeImage(img){
-  const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  // WHAT TYPE OF REQUEST?
+        body: img,  
 
-  const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
-
-  let params = new URLSearchParams({
-    'returnFaceId': 'true',
-    'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
-  })
-  let resp = await fetch(uriBase + '?' + params.toString(), {
-    method: 'POST',  //WHAT TYPE OF REQUEST?
-    body: img,  //WHAT ARE WE SENDING TO THE API?
-    headers: {
-        'Content-Type': 'application/octet-stream',  //do this in the next section
-        'Ocp-Apim-Subscription-Key': subscriptionKey
-    }
-  })
-  let data = await resp.json();
-
-  return data; 
-
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key' : subscriptionKey
+        }
+    })
+    let data = await resp.json();
+    
+    return data; 
 }
