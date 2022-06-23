54289195b29b7ac7e7e7d7f733c74e08b9676b80
Steven Yeung <steveny1215@gmail.com>
2022-06-23 11:34:49 -0400

    can run f5

--- week1/emotionalgifs/index.js
+++ week1/emotionalgifs/index.js
@@ -1,53 +1,51 @@
 const multipart = require('parse-multipart');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    // here's your boundary:
     const boundary = multipart.getBoundary(req.headers['content-type']);
   
-    // Assigns the body variable the correct value
+    // TODO: assign the body variable the correct value
     const body = req.body;
 
-    // parses the body
-    const parts = multipart.Parse(body, boundary);
+    // parse the body
+    const parts = multipart.Parse(body, boundary);    
 
-    // module.exports function
-    // analyzes the image
     const result = await analyzeImage(parts[0].data);
 
-    
     let emotions = result[0].faceAttributes.emotion;    
     let objects = Object.values(emotions);
     const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
 
-    context.res = {
-        body: {
-          //result
-          main_emotion
 
-        }
-    };
-    console.log(result)
-    context.done(); 
+context.res = {
+	body: {
+		main_emotion
+	}
+};
+console.log(result)
+context.done(); 
 }
 
-async function analyzeImage(img) {
-    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+async function analyzeImage(img){
+    //const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const subscriptionKey = "2b46dc89f4624d6ba01b0b629dd94ad0"; //process.env.SUBSCRIPTIONKEY;
 
+    //const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    const uriBase = " https://placeholdeer-face-api.cognitiveservices.azure.com" + "/face/v1.0/detect";
     let params = new URLSearchParams({
         'returnFaceId': 'true',
-        'returnFaceAttributes': 'emotion'  //FILL IN THIS LINE
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
     })
 
     let resp = await fetch(uriBase + '?' + params.toString(), {
-        method: 'POST',  // WHAT TYPE OF REQUEST?
-        body: img,  
-
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
         headers: {
-            'Content-Type': 'application/octet-stream',
-            'Ocp-Apim-Subscription-Key' : subscriptionKey
+            'CONTENT-TYPE': 'application/octet-stream',  //do this in the next section
+            "Ocp-Apim-Subscription-Key": subscriptionKey
         }
     })
     let data = await resp.json();
